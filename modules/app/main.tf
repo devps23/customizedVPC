b1a6b149a36630f3228fdf41795b3fbe15bd1538
Pavani <vemasani.pavani@gmail.com>
2024-04-09 01:32:19 +0530

    routetable id variable has changed

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -39,8 +39,8 @@ resource "aws_route_table" "router" {
     Name = var.routename
   }
 }
-resource "aws_route" "vpc_edit_route" {
-  route_table_id = aws_route_table.router.vpc_id
+resource "aws_route" "editroute" {
+  route_table_id = aws_route.editroute.route_table_id
   destination_cidr_block     = var.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.peer_vpc_id
 }
