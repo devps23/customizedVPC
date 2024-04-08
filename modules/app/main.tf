bf683549edae9435ffcbeea02c56e180f1fcc479
Pavani <vemasani.pavani@gmail.com>
2024-04-09 01:21:55 +0530

    destination_cidr_block changed the name

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -41,7 +41,7 @@ resource "aws_route_table" "router" {
 }
 resource "aws_route" "vpc_edit_route" {
   route_table_id = aws_route_table.router.vpc_id
-  cidr_block = var.cidr_block
+  destination_cidr_block     = var.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.peer_vpc_id
 }
 //resource "aws_vpc" "default_edit_route" {
