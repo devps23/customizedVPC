eaed3b61eea118980351ed5fe3d0421cc92d50cd
Pavani <vemasani.pavani@gmail.com>
2024-04-09 00:03:11 +0530

    peer name changed....

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -1,11 +1,11 @@
-resource "aws_instance" "test" {
-  ami           = var.ami
-  instance_type = var.instancetype
-  vpc_security_group_ids = [data.aws_security_group.security.id]
-  tags = {
-    Name = var.instancename
-  }
-}
+//resource "aws_instance" "test" {
+//  ami           = var.ami
+//  instance_type = var.instancetype
+//  vpc_security_group_ids = [data.aws_security_group.security.id]
+//  tags = {
+//    Name = var.instancename
+//  }
+//}
 data "aws_security_group" "security"{
   name = "allow-all"
 }
