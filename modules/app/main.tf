7442e09ea082ebbab4432e116c054bc68838aa8c
Pavani <vemasani.pavani@gmail.com>
2024-04-10 10:39:29 +0530

    add route for custom vpc to default vpc

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -15,6 +15,7 @@ resource "aws_vpc" "vpc" {
     Name = var.vpcname
   }
 }
+//[id=vpc-005b54b715e988e73]
 resource "aws_subnet" "subnet_dev" {
   vpc_id     = aws_vpc.vpc.id
   cidr_block = var.cidr_block
@@ -23,6 +24,7 @@ resource "aws_subnet" "subnet_dev" {
     Name = var.subnet_name
   }
 }
+//[id=subnet-0a28345179c03762b]
 resource "aws_vpc_peering_connection" "peer" {
   peer_vpc_id   = var.requester_vpc_id
   vpc_id        = aws_vpc.vpc.id
@@ -31,8 +33,12 @@ resource "aws_vpc_peering_connection" "peer" {
   tags = {
     Name = var.peer_conn
   }
-
+  //[id=pcx-036439135bf21c6be]
 }
+resource "aws_route_table" "route" {
+  vpc_id = aws_vpc.vpc.id
+}
+
 //resource "aws_route_table" "router" {
 //  vpc_id = aws_vpc.vpc.id
 //  tags = {
@@ -47,13 +53,14 @@ resource "aws_vpc_peering_connection" "peer" {
 //  //peerconnection
 //  vpc_peering_connection_id =  aws_vpc_peering_connection.peer.id//custom
 //}
-//resource "aws_route" "cust_editroute" {
-//  //resource routetable id
-//  route_table_id = aws_vpc.vpc.default_route_table_id
-//// for aws route table id with default vpc  peer connection
-//  destination_cidr_block     = var.default_vpc_cidr_block
-//  vpc_peering_connection_id = "pcx-04007ddaacb9d7c26"
-//}
+resource "aws_route" "cust_editroute" {
+  //resource routetable id
+  route_table_id = aws_vpc.vpc.default_route_table_id
+  vpc_peering_connection_id = "pcx-04007ddaacb9d7c26"
+// for aws route table id with default vpc  peer connection
+  destination_cidr_block     = var.default_vpc_cidr_block
+
+}
 //
 //output "custompeer" {
 //  value = aws_vpc_peering_connection.peer.id
