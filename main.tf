f7a7ecc633fbf1f460734ceff6c22e66d6bcaa7c
Your Name <vemasani.pavani@gmail.com>
2025-04-21 13:30:45 +0530

    eks

--- main.tf
+++ main.tf
@@ -16,10 +16,8 @@
 module "eks"{
   source                    =  "./modules/eks"
   env                       =  var.env
-#   subnet_id                 =  module.vpc.backend_subnets
   component                 = "eks-cluster"
   kms_key_id                = var.kms_key_id
-#   vpc_id                    = module.vpc.vpc_id
   bastion_nodes             = var.bastion_nodes
 }
 
